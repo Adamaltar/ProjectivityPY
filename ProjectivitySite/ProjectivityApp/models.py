d7da902cbcbea8757491a15780a9f2d5d0a327ac
Laura M <92090769+MarinLauraSmaranda@users.noreply.github.com>
2023-06-27 05:28:39 +0300

    Update models.py

--- ProjectivitySite/ProjectivityApp/models.py
+++ ProjectivitySite/ProjectivityApp/models.py
@@ -36,13 +36,22 @@ class UtilizatorManager(BaseUserManager):
         return user
 
 
+class Departament(models.Model):
+    cod_departament = models.AutoField(primary_key=True)
+    nume_departament = models.CharField(max_length=50)
+
+
+class Echipe(models.Model):
+    cod_echipa = models.AutoField(primary_key=True)    
+    nume_echipa = models.CharField(max_length=50)
+    cod_departament = models.ForeignKey(Departament, on_delete=models.CASCADE, default='')
 
 class Utilizator(AbstractBaseUser, PermissionsMixin):
-    cod_utilizator = models.TextField(max_length=50, unique=True, null=True)
+    cod_utilizator = models.TextField(max_length=50, unique=True, default='')
     nume = models.TextField(max_length=50, blank=True)
     prenume = models.TextField(max_length=50, blank=True)
-    cod_departament = models.IntegerField(blank=True, null=True, default=None)
-    cod_echipa = models.IntegerField(blank=True, null=True, default=None)
+    cod_departament = models.ForeignKey(Departament, on_delete=models.CASCADE, default='')
+    cod_echipa = models.ForeignKey(Echipe, on_delete=models.CASCADE, default='')
     parola = models.TextField(max_length=50, blank=True)
     adresa_email = models.EmailField(_('adresa email'), unique=True)
     nr_telefon = models.CharField(max_length=15)
@@ -67,41 +76,6 @@ class Utilizator(AbstractBaseUser, PermissionsMixin):
 # def nume_complet(self):
 #     return f"{self.nume} {self.prenume}"
 
-# def to_python(self, value):
-#     if value is None:
-#         return None
-#     return str(value)
-
-    # @classmethod
-    # def create_multiple_users(cls, users_data):
-    #     for user_data in users_data:
-    #         user = cls(
-    #             cod_utilizator=user_data['cod_utilizator'],
-    #             nume=user_data['nume'],
-    #             prenume=user_data['prenume'],
-    #             cod_departament=user_data['cod_departament'],
-    #             cod_echipa=user_data['cod_echipa'],
-    #             parola=user_data['parola'] or '',
-    #             adresa_email=user_data['adresa_email'],
-    #             nr_telefon=user_data['nr_telefon'],
-    #             locatie_birou=user_data['locatie_birou'],
-    #             atributii=user_data['atributii'],
-    #             notificari=user_data['notificari'],
-    #         )
-    #         user.save()
-
-
-class Departament(models.Model):
-    cod_departament = models.AutoField(primary_key=True)
-    nume_departament = models.CharField(max_length=50)
-    cod_echipa = models.IntegerField()
-
-
-class Echipe(models.Model):
-    cod_echipa = models.AutoField(primary_key=True)    
-    nume_echipa = models.CharField(max_length=50)
-    cod_departament = models.IntegerField()
-
 
 class Proiect(models.Model):
     cod_proiect = models.AutoField(primary_key=True)
@@ -110,7 +84,7 @@ class Proiect(models.Model):
     data_inceput = models.DateField()
     data_sfarsit = models.DateField()
     status = models.CharField(max_length=15)
-    cod_departament = models.IntegerField()
+    cod_departament = models.ForeignKey(Departament, on_delete=models.CASCADE, default='')
 
 
 class Task(models.Model):
@@ -119,8 +93,8 @@ class Task(models.Model):
     descriere = models.CharField(max_length=50)
     data = models.DateField()
     status = models.CharField(max_length=15)
-    cod_departament = models.IntegerField()
-    cod_echipa = models.IntegerField()
+    cod_departament = models.ForeignKey(Departament, on_delete=models.CASCADE, default='')
+    cod_echipa = models.ForeignKey(Echipe, on_delete=models.CASCADE, default='')
 
 
 class Sedinta(models.Model):
@@ -130,25 +104,32 @@ class Sedinta(models.Model):
     data_sedinta = models.DateField()
     ora_inceput = models.TimeField()
     ora_sfarsit = models.TimeField()
-    cod_departament = models.IntegerField()
+    cod_departament = models.ForeignKey(Departament, on_delete=models.CASCADE, default='')
 
 
 class Chat(models.Model):
-    cod_chat = models.AutoField(primary_key=True)
-    nume_chat = models.CharField(max_length=50)
+    cod_chat = models.AutoField(primary_key=True)   
+    cod_utilizator = models.ForeignKey(Utilizator, on_delete=models.CASCADE, max_length=50, default='')
+    nume_chat = models.CharField(max_length=200)
     continut = models.TextField()
+    
+    def __str__(self):
+        return self.nume_chat    
 
 
 class Mesaj(models.Model):
     cod_mesaj = models.AutoField(primary_key=True)
     mesaj = models.CharField(max_length=100)
     data_mesaj = models.DateTimeField()
-    cod_utilizator = models.IntegerField()
-    cod_chat = models.IntegerField()
+    cod_utilizator = models.ForeignKey(Utilizator, on_delete=models.CASCADE, max_length=50, default='')
+    cod_chat = models.ForeignKey(Chat, on_delete=models.CASCADE, default='')
 
+    def __str__(self):
+        return self.mesaj    
 
 class Forum(models.Model):
     cod_forum = models.AutoField(primary_key=True)
+    cod_utilizator = models.ForeignKey(Utilizator, on_delete=models.CASCADE, max_length=50, default='')
     nume_forum = models.CharField(max_length=50)
     descriere = models.TextField()
 
@@ -158,14 +139,14 @@ class Postare(models.Model):
     titlu = models.CharField(max_length=100)
     continut = models.TextField()
     data_postarii = models.DateField()
-    cod_utilizator = models.IntegerField()
-    cod_forum = models.IntegerField()
+    cod_utilizator = models.ForeignKey(Utilizator, on_delete=models.CASCADE, max_length=50, default='')
+    cod_forum = models.ForeignKey(Forum, on_delete=models.CASCADE, default='')
 
 
 class Comentariu(models.Model):
     cod_comentariu = models.AutoField(primary_key=True)
     continut = models.TextField()
     data_comentariului = models.DateField()
-    cod_utilizator = models.IntegerField()
-    cod_postare = models.IntegerField()
+    cod_utilizator = models.ForeignKey(Utilizator, on_delete=models.CASCADE, max_length=50, default='')
+    cod_postare = models.ForeignKey(Postare, on_delete=models.CASCADE, default='')
 
